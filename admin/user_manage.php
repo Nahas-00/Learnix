16de6e4a6c014216244b753926d14d63b3906674
akhilva234 <akhilva10a@gmail.com>
2025-07-07 16:20:35 +0530

    Separated dashboard sessions

---
+++ admin/user_manage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin | User Manage</title>
+    <link rel="stylesheet" href="../styles/admin_dash.css">
+</head>
+<body>
+    <div class="welcome-message">
+     <div class="welcome-header">
+       <h1>Manage Users</h1>
+      <p class="welcome-text">Add, Edit or Delete User</p>
+     </div>
+</body>
+</html>
