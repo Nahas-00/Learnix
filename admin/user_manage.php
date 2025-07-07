b922e365f0b003e0eb5c70653db4993a69107120
Nahas-00 <nahas1021@gmail.com>
2025-07-07 23:17:17 +0530

    Created user add option in admin dashboard , its incomplete

--- admin/user_manage.php
+++ admin/user_manage.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin | User Manage</title>
     <link rel="stylesheet" href="../styles/admin_dash.css">
+    <link rel="stylesheet" href="../styles/add-user.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
 </head>
 <body>
     <div class="welcome-message">
@@ -12,5 +14,46 @@
        <h1>Manage Users</h1>
       <p class="welcome-text">Add, Edit or Delete User</p>
      </div>
+
+      <div class="profile-icon">
+        <img src="../uploads/no_profile.png" alt="">
+        <span class="profile-span">View profile</span>
+      </div>
+    </div>
+
+
+    <div class="overlay" id="overlay"></div>
+        <button onclick="openAddUser()">Add User</button>
+        <!--Add user form-->
+
+    <div class="add-user" id="add-user">
+        <div class="add-user-title">
+            <h1>Add User</h1>
+            <i class="fa-solid fa-xmark" onclick="closeAddUser()"></i>
+        </div>
+        
+        <form action="add-user.php" class="user-form">
+            <div class="username">
+            <label for="username">Username</label>
+            <input type="text" placeholder="eg: John Doe">
+            </div>
+
+            <div class="email">
+                <label for="username">Email</label>
+                <input type="text" placeholder=" eg: test@gmail.com">
+            </div>
+
+            <div class="password">
+                <label for="username">Password</label>
+                <input type="password" placeholder="Atleast 5 character nad minimum 1 number">
+            </div>
+
+            <div class="submit-btn">
+                <input type="submit" value="Add" name="submit">
+            </div>
+        </form>
+    </div>
+
+    <script src="../scripts/add-user.js"></script>
 </body>
 </html>
---
+++ scripts/add-user.js
@@ -0,0 +1,11 @@
+
+function openAddUser() {
+  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('add-user').style.display = 'block';
+}
+
+
+function closeAddUser() {
+  document.getElementById('overlay').style.display = 'none';
+  document.getElementById('add-user').style.display = 'none';
+}
