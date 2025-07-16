1ef64c497aedd564c0f2ceca9a8d88c75443e4a7
Nahas-00 <nahas1021@gmail.com>
2025-07-16 22:49:47 +0530

    Styled displaying of question info

--- scripts/showQuestion.js
+++ scripts/showQuestion.js
@@ -1,7 +1,14 @@
   function showCode(button){
   document.getElementById('code-overlay').style.display = 'block';
   document.getElementById('code-dis').style.display = 'flex';
-  document.getElementById('code-area').textContent = button.dataset.title + button.dataset.description;
+    document.getElementById('ques-title').textContent = button.dataset.title; 
+  document.getElementById('ques-cat').textContent = button.dataset.category; 
+  document.getElementById('ques-topic').textContent = button.dataset.name; 
+  document.getElementById('ques-diff').textContent = button.dataset.difficulty; 
+  document.getElementById('ques-des').textContent = button.dataset.description; 
+  document.getElementById('ques-solution').textContent = button.dataset.solution; 
+  document.getElementById('ques-hint').textContent = button.dataset.hint; 
+    document.getElementById('ques-test-case').textContent = button.dataset.input + button.dataset.output;
 }
 
 function closeCode(){
