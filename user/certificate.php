7a51e5787f4358e2ca89117e82ace7de7b47f4c8
Nahas-00 <nahas1021@gmail.com.com>
2025-08-13 12:43:02 +0530

    Certificate page designing

--- user/certificate.php
+++ user/certificate.php
@@ -3,9 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>certificate</title>
+  <link rel="stylesheet" href="../assets/admin_dash.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
+  <link rel="stylesheet" href="../assets/certificate.css">
 </head>
 <body>
-  Hello
+
+    <div class="name-field">
+      <form action="" method="post">
+        <div>
+      <input type="text" name="name" placeholder="Enter name to print on certificate">
+        </div>
+        <div>
+      <input type="submit" name="Submit" class="btn">
+      </div>
+      </form>
+    </div>
 </body>
 </html>
